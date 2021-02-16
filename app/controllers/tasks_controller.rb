0d23de50f1ca22e9c377c024b478cdd92ab53509
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-02-16 13:52:55 -0500

    Added index, show, new, create, private task_params

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,31 @@
+class TasksController < ApplicationController
+  # * List out all tasks on /tasks
+  def index
+    @tasks = Task.all
+  end
+
+  # * Display individual task on /tasks/#{id}
+  def show
+    @task = Task.find(params[:id])
+  end
+  
+  # * Add new instance for new task
+  def new
+    @task = Task.new
+  end
+  
+  # * Create and save new instance of task
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    # ! Strong params
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # * List tasks
+  get 'tasks', to: 'tasks#index'
+
+  # * Display new task form
+  get 'tasks/new', to: 'tasks#new'
+
+  # * View details of a task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # * Handle POST request generated on form submission
+  post 'tasks', to: 'tasks#create'
+
 end
