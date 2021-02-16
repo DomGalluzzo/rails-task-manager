05e6256937f4098608724d33e65db5769b6f1450
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-02-16 15:31:24 -0500

    Added patch to edit task

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,21 @@ Rails.application.routes.draw do
   # * List tasks
   get 'tasks', to: 'tasks#index'
 
-  # * Display new task form
-  get 'tasks/new', to: 'tasks#new'
-
-  # * View details of a task
-  get 'tasks/:id', to: 'tasks#show', as: :task
-
   # * Handle POST request generated on form submission
   post 'tasks', to: 'tasks#create'
+
+  # * New task form
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  
+  # * Edit a single task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+
+  # * Show a single task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # * Update specified task
+  patch 'tasks/:id', to: 'tasks#update'
+
+
 
 end
