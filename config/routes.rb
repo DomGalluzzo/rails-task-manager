f96ccf5b2716b54d7945fe1b7853c1bc40eefdef
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-02-16 15:42:39 -0500

    Added delete route. Reformatted routes with resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,23 @@
 Rails.application.routes.draw do
-  # * List tasks
-  get 'tasks', to: 'tasks#index'
+  # # * List tasks
+  # get 'tasks', to: 'tasks#index'
 
-  # * Handle POST request generated on form submission
-  post 'tasks', to: 'tasks#create'
+  # # * Handle POST request generated on form submission
+  # post 'tasks', to: 'tasks#create'
 
-  # * New task form
-  get 'tasks/new', to: 'tasks#new', as: :new_task
+  # # * New task form
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
   
-  # * Edit a single task
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # # * Edit a single task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
 
-  # * Show a single task
-  get 'tasks/:id', to: 'tasks#show', as: :task
+  # # * Show a single task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
 
-  # * Update specified task
-  patch 'tasks/:id', to: 'tasks#update'
+  # # * Update specified task
+  # patch 'tasks/:id', to: 'tasks#update'
 
-
-
+  # # * Remove task
+  # delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 end
